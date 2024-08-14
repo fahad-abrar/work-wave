90139208a2e07f416404e283afe935f97692e0b9
fahad-abrar <fahad.abrarr@gmail.com>
2024-08-14 09:19:21 +0600

    add token functionality

--- controller/userController.js
+++ controller/userController.js
@@ -6,6 +6,7 @@ import bcrypt from 'bcryptjs'
 import multerUploader from '../utils/fileHandler.js'
 import fs from 'fs'
 
+
 class authUser{
 
     static async getUser(req, res){
@@ -64,17 +65,20 @@ class authUser{
                 // Check if the user already exists
                 const existUser = await User.findOne({ email });
                 if (existUser) {
-                    return res.status(200).json({
+                    return res.status(400).json({
                         success: false,
                         message: 'Email is already used',
                     });
                 }
+                // generating hash password
+                const salt = bcrypt.genSaltSync(10)
+                const hashPass = bcrypt.hashSync(password, salt)
         
                 // Prepare the user data
                 const userData = { 
                     name, 
                     email, 
-                    password,
+                    password: hashPass,
                     phone,
                     address,
                     niche:{
@@ -120,11 +124,60 @@ class authUser{
         
 
     }
+
+
     static async logInUser(req, res){
         try {
-            return res.status(200).json({
+            const {email, password} = req.body
+
+            // find the user if the user is exist
+            const findUser = await User.findOne({email})
+            if(!findUser){
+                return res.status(400).json({
+                    success: false,
+                    message: 'invalid email or password'
+                    })
+                }
+            
+            
+            // Check that the password in the database is a string
+            if (typeof findUser.password !== 'string') {
+                console.log('Stored password is not a string:', findUser.password);
+                return res.status(400).json({
+                    success: false,
+                    message: 'Invalid email or password',
+                });
+            }
+
+            // check given password is correct or not
+            const isMatch = bcrypt.compareSync(password, findUser.password);
+
+            // const isMatch = bcrypt.compareSync(password, findUser.password)
+            if(!isMatch){
+                return res.status(400).json({
+                    success: false,
+                    message: 'invalid email or password'
+                    })
+                }
+            
+            // generate a payload to store user data in token
+            const payload = {
+                id: findUser._id,
+                userName: findUser.name,
+                email: findUser.email
+            }
+
+            //generate a token
+            const token = jwt.sign(payload, process.env.JWT_SECRET,{
+                expiresIn:"365d"
+            })
+
+            console.log(token)
+            //const option={ }
+            return res.cookie('token', token).status(200).json({
                 success: true,
-                message:' user get successfully'
+                message:' user get successfully',
+                token
             })
             
         } catch (error) {
@@ -136,6 +189,8 @@ class authUser{
         }
 
     }
+
+
     static async logOutUser(req, res){
         try {
             return res.status(200).json({
--- router/index.js
+++ router/index.js
@@ -4,8 +4,14 @@ import multerUploader from '../utils/fileHandler.js'
 const router =express.Router()
 
 // register router
-router.get('/register', authUser.getUser )
+router.get('/auth/user', authUser.getUser )
+router.get('/auth/user/:id', authUser.getUser )
 router.post('/register',multerUploader, authUser.registerUser )
+router.post('/auth/login', authUser.logInUser )
+router.get('/auth/logout', authUser.logOutUser )
+router.put('/auth/update/:id', authUser.updateUser )
+router.delete('/auth/delete/:id', authUser.deleteUser )
+router.put('/auth/updatepassword', authUser.getUser )
 
 
 
